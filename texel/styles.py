74ab90f7e442e4d25cd860c4699761396b0d573a
late <lauri@port6.io>
2022-02-05 01:04:03 +0200

    Update gitignore

--- texel/styles.py
+++ texel/styles.py
@@ -3,6 +3,16 @@ import math
 import numpy as np
 
 
+class C:
+    WHITE = curses.COLOR_WHITE
+    BLACK = curses.COLOR_BLACK
+    BLUE = curses.COLOR_BLUE
+    GREEN = curses.COLOR_GREEN
+    YELLOW = curses.COLOR_YELLOW
+    MAGENTA = curses.COLOR_MAGENTA
+    LIGHT_MAGENTA = 213
+
+
 class Styles:
     def __init__(self, cellwidth: int, precision: int):
         self.separator: str = "|"
@@ -19,15 +29,16 @@ class Styles:
         self.lineno_fmt: str = self.get_lineno_format_string()
 
     def init_colors(self):
-        curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLUE)
-        curses.init_pair(2, curses.COLOR_MAGENTA, curses.COLOR_BLACK)
-        curses.init_pair(3, curses.COLOR_BLUE, curses.COLOR_BLACK)
-        curses.init_pair(4, curses.COLOR_GREEN, curses.COLOR_BLACK)
-        curses.init_pair(5, curses.COLOR_BLACK, curses.COLOR_YELLOW)
+        curses.init_pair(1, C.WHITE, C.BLUE)
+        curses.init_pair(2, C.MAGENTA, C.BLACK)
+        curses.init_pair(3, C.BLUE, C.BLACK)
+        curses.init_pair(4, C.GREEN, C.BLACK)
+        curses.init_pair(5, C.BLACK, C.YELLOW)
+        curses.init_pair(6, C.LIGHT_MAGENTA, C.BLACK)
         self.c_selection = curses.color_pair(1)
         self.c_sheets = curses.color_pair(2)
         self.c_border = curses.color_pair(3)
-        self.c_sheet_selection = curses.A_UNDERLINE | curses.color_pair(4)
+        self.c_sheet_selection = curses.A_UNDERLINE | curses.color_pair(6)
         self.c_header = curses.A_UNDERLINE | self.c_border
         self.c_footer = curses.A_UNDERLINE | curses.A_BOLD | curses.color_pair(4)
         self.c_lineno = self.c_border
